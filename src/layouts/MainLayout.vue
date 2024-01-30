33d48918862fcd1093a0b51209703375740f840e
katsuro666 <kreepinoutdakut@gmail.com>
2024-01-30 13:52:56 +0300

    feat: add header to the main layout

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,14 +1,48 @@
 <template>
-  <div>
-    tut budet header
-    <router-view />
+  <div class="header">
+    <router-link :to="RoutePaths.MAIN" class="header-link" activeClass="header-link-active">Все котики</router-link>
+
+    <router-link :to="RoutePaths.FAVORITES" class="header-link" activeClass="header-link-active">
+      Любимые котики
+    </router-link>
   </div>
+  <router-view />
 </template>
 
 <script setup lang="ts">
-
+import { RoutePaths } from '../constants/route.constants';
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+.header {
+  display: flex;
+  align-items: center;
+  background-color: $primary;
+  height: 4rem;
+  padding: 0 4rem;
 
+  @media (max-width: $breakpoint-mobile) {
+    padding: 1rem;
+    justify-content: space-between;
+  }
+}
+
+.header-link {
+  height: 100%;
+  display: flex;
+  align-items: center;
+  padding: 0 1rem;
+  color: $text-secondary;
+  transition: all ease 0.3s;
+
+  &-active {
+    background: $accent;
+    color: $text-accent;
+  }
+
+  &:hover {
+    cursor: pointer;
+    box-shadow: 0 3px 20px 0 rgba(0, 0, 0, 0.2);
+  }
+}
 </style>
